e7fbffa69c7034bbef4c0502d69abebb19ff1830
buituht <buituht@gmail.com>
2025-09-27 14:03:04 +0700

    Add files via upload

--- WebChervlet/src/main/java/vn/iotstar/daos/CategoryDao.java
+++ WebChervlet/src/main/java/vn/iotstar/daos/CategoryDao.java
@@ -1,25 +1,19 @@
 package vn.iotstar.daos;
 
 import java.util.List;
-
 import vn.iotstar.models.CategoryModel;
 
 public interface CategoryDao {
-
-	List<CategoryModel> getAll();
-
-	CategoryModel get(int id);
-
-	void delete(int id);
-
-	void edit(CategoryModel category);
-
-	void insert(CategoryModel category);
-	
-	CategoryModel findByCategoryName(String cate_name);
-	
-	
-
-
-	
+    
+    List<CategoryModel> findAll();
+    
+    CategoryModel get(int catId);
+    
+    void insert(CategoryModel category);
+    
+    void edit(CategoryModel category);
+    
+    void delete(int catId);
+    
+    CategoryModel findByCategoryName(String cateName);
 }
